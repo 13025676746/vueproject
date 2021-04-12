7a33ad34840af293c90ab64b560bf93c45b2677c
wuzhuchenng <844577475@qq.com>
2021-04-12 09:56:12 +0800

    'init'

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import actions from "@/store/actions";
+import getters from "@/store/getters";
+import mutations from "@/store/mutations";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  getters:getters,
+  mutations: mutations,
+  actions: actions,
 })
